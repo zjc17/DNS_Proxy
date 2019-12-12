bfcab47ed9f25d02c385c51a5df7896647cf4134
Jiachen-WSL <acezjc@foxmail.com>
2019-12-12 23:53:12 +0800

    update

---
+++ src/core/Proxy_TUN.py
@@ -0,0 +1,20 @@
+#!./venv python3
+# -*- coding: utf-8 -*-
+'''
+管理 TUN interface, 需要管理员权限
+'''
+
+from tuntap import TunTap
+
+class TUN:
+    def __init__(self, nic_type, nic_name):
+        self.__type = nic_type
+        self.__name = nic_name
+        self.__TUN = TunTap(nic_type=self.__type, nic_name=self.__name)
+        print("Successfully create the TUN interface", 
+                self.__TUN.name,self.__TUN.ip,self.__TUN.mask)
+
+
+if __name__ == '__main__':
+    tun = TUN(nic_type="Tun",nic_name="tun0")
+    pass
