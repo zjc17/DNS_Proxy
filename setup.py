d32c05d54de6b32ddec61088943ad9fd3836325b
Jiachen <acezjc@foxmail.com>
2019-12-18 23:48:45 +0800

    Jc dev enhance (#8)
    * 代理服务端和客户端成功建立隧道 (#7)
    
    * update
    
    * update
    
    * update
    
    * update receiver
    
    * update test_tun
    
    * update Server and Client
    
    * update
    
    * 优化会话管理功能
    
    * rewrite the action of server
    
    * 重构服务端，重新定义交互消息结构
    
    * refactor server
    
    * start refactor client
    
    * rm unused file
    
    * refactor client
    
    * fix compile error
    
    * network connected
    
    * network connected
    
    * 网络联通，但是project给的NS服务器响应发包速度过快，需要有限过滤包
    
    * flag1
    
    * flag1

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 import os
+import site
 PROJECT_NAME = 'DNS_Proxy'
 with open('./bin/' + PROJECT_NAME + '.pth', 'w') as f:
     f.write(os.getcwd() + '\n')
     f.write(os.getcwd() + '/src\n')
     f.close()
-import site
 os.system('cp ./bin/' + PROJECT_NAME + '.pth ' + site.getsitepackages()[0])
